faf528f58d1e1a4e70e90fdbb1c55bb72b034271
alisazou <513242026@qq.com>
2016-10-28 19:13:09 +0800

    添加mongodb

---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports = { 
+  cookieSecret: 'xm', //cookieSecret 用于 Cookie 加密与数据库无关，我们留作后用
+  db: 'xm', //其中 db 是数据库的名称
+  host: 'localhost',//host 是数据库的地址
+  port: 27017//port是数据库的端口号
+}; 
